c0251a46f65d69d2bf48a0f9785878e8c3f3c2f7
helen-m-lin <helen.m.lin@mail.mcgill.ca>
2019-01-26 22:43:56 -0500

    Changed correction code. New correction algorithm accounts for each type of turn and checks that a complete square was crossed. Works.

--- Lab2/src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
+++ Lab2/src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
@@ -8,19 +8,21 @@ import lejos.robotics.SampleProvider;
 
 public class OdometryCorrection implements Runnable {
   private enum CorrectionType{HEADING, DISTANCE}
-  private static final float DIST_THRESHOLD = 5;
+  private static final float DIST_THRESHOLD = 5; //cm
+  
   private static final CorrectionType CORRECTION = CorrectionType.DISTANCE;
-  private static final float LIGHT_THRESHOLD = 0.28f; //lowered light threshold 
-  private static final int T_THRESHOLD = 15;
+ 
+  private static final float LIGHT_THRESHOLD = 0.28f; //lowered light threshold (detects black lines)
+  private static final int T_THRESHOLD = 10; //theta threshold (degrees)
   private static final float LINE_SPACING = 30.48f;
   private static final long CORRECTION_PERIOD = 10;
   
   
   private Odometer odometer;
   private SampleProvider lightSensor;
-  private float[] sample;
+  private float[] sample; //store light sensor sample
   
-  private double[] lastPos;
+  private double[] lastPos; //store XYT at last line
   
   
   /**
@@ -39,11 +41,9 @@ public class OdometryCorrection implements Runnable {
   }
 
   /**
-   * Here is where the odometer correction code should be run.
-   * 
+   * run method for odometer correction (required for Thread)
    * @throws OdometerExceptions
    */
-  // run method (required for Thread)
   public void run() {
 	int lineCount = 0;
     long correctionStart, correctionEnd;
@@ -52,27 +52,29 @@ public class OdometryCorrection implements Runnable {
       correctionStart = System.currentTimeMillis();
 
       // TODO Trigger correction (When do I have information to correct?)
+      lightSensor.fetchSample(sample, 0);
       
       //TODO: FILTER LIGHT SENSOR!!! MAKE SURE YOU"VE moved first
+      //done by checking if light sensor sample is less than the black line detection threshold
       
-      
-      lightSensor.fetchSample(sample, 0);
      
       //To avoid a single line triggering this many times, verify that either
       //we haven't seen a line yet at all (lastPos == null) or we're sufficiently
       //far from the last line.
       if (sample[0] < LIGHT_THRESHOLD && 
     		  (lastPos == null || dist(lastPos, odometer.getXYT()) > DIST_THRESHOLD)) {
-    	  lineCount++;
-    	 
+    	  
+    	  lineCount++; //line detected
+    	  Lab2.lcd.drawString(lineCount + " line(s) detected.", 0, 5);
+    	  
     	  // TODO Calculate new (accurate) robot position
-    	  if (lastPos == null) {
+    	  if (lastPos == null) { //first line
     		  lastPos = odometer.getXYT();
     	  } else {
     		  //we have now reached a second line, ideally perpendicular to our trajectory!
 
               // TODO Update odometer with new calculated (and more accurate) values
-    		  double[] pos = odometer.getXYT();
+    		  double[] pos = odometer.getXYT(); //current odo-position
     		 
     		  //heading correction
     		  if (CORRECTION == CorrectionType.HEADING) {
@@ -88,6 +90,8 @@ public class OdometryCorrection implements Runnable {
     							  	Math.pow(lastPos[1] - pos[1], 2));
     			  //this should be true! If it isn't true, there is necessarily an error
     			  //in the distance reading, or we crossed a non-perpendicular line.
+    			  
+    			  
     			  if (predictedDistance > LINE_SPACING) {
     				  double tError = Math.toDegrees(Math.acos(predictedDistance/LINE_SPACING));
     				  //if we have some way of knowing the tendancy of the robot, we can choose to
@@ -99,35 +103,60 @@ public class OdometryCorrection implements Runnable {
     			  
     		  } 
     		  
-    		  //distance correction
-    		  else if (CORRECTION == CorrectionType.DISTANCE) {
-    			  //Here, we assume the angle reading was sufficient, and scale distance
-    			  //System.out.println("correcting");
+    		  //distance correction (note: cannot correct the first line going in any direction because starts from centre
+    		  if (CORRECTION == CorrectionType.DISTANCE) {
     			  
+    			  //1) upward (+Y) path: theta=0, y>x
+    			 if ((pos[2]<T_THRESHOLD || (360-pos[2]<T_THRESHOLD) )&& pos[1]>pos[0]){  //theta approximately 0deg	 
+    				  odometer.setY(lastPos[1]+LINE_SPACING); //correct Y-value
+    				  Lab2.lcd.drawString(lineCount + " line(s) detected.\nMoved UP.", 0, 5); //display check	
+    			  }
+    			  //2) rightward (+X) path: theta=90, y>x
+    			  else if (Math.abs(pos[2]-90)<T_THRESHOLD && (pos[1]>pos[0]) && (pos[0]-lastPos[0]>LINE_SPACING-DIST_THRESHOLD)){ 
+    				  odometer.setX(lastPos[0]+LINE_SPACING); //correct X-value
+    				  Lab2.lcd.drawString(lineCount + " line(s) detected.\nMoved RIGHT.", 0, 5); //display check
+    				 
+    			  }
+    			 //3) downward (-Y) path: theta = 180, x>y
+    			  else if (Math.abs(pos[2]-180)<T_THRESHOLD && pos[0]>pos[1] && (lastPos[1]-pos[1]>LINE_SPACING-DIST_THRESHOLD)){  
+    				  odometer.setY(lastPos[1]-LINE_SPACING); //correct Y-value
+    				  Lab2.lcd.drawString(lineCount + " line(s) detected.\nMoved DOWN.", 0, 5); //display check
+    			 }
+    			  //4)leftward path theta = 270, x>y
+    			  else if (Math.abs(pos[2]-270)<T_THRESHOLD && pos[0]>pos[1] && (lastPos[0]-pos[0]>LINE_SPACING-DIST_THRESHOLD)){
+     				 	  odometer.setX(lastPos[0]-LINE_SPACING); //correct Y-value
+      					  Lab2.lcd.drawString(lineCount + " line(s) detected.\nMoved LEFT.", 0, 5); //display check
+       			 }
+    			  
+    			  //finished correcting odometer, set currrent position as lastPos
+    			  lastPos = pos;
+    			  
+    			  //old correction code. commented out for now.
     			  //these multipliers should be elements of {-1,1,0}
-    			  int sin = (int) (Math.sin(Math.toRadians(pos[2]) + 0.5));
-    			  int cos = (int) (Math.cos(Math.toRadians(pos[2]) + 0.5));
-    			  if (sin == 0) {
-    				  //y motion
-    				  odometer.setXYT(pos[0], lastPos[1] + sin * LINE_SPACING, pos[2]);
-    			  } else {
-    				  //x change
-    				  odometer.setXYT(lastPos[1] + cos * LINE_SPACING, pos[1], pos[2]);
-    				 // System.out.println("corrected");
+    			//  int sin = (int) (Math.sin(Math.toRadians(pos[2]) + 0.5));
+    			//  int cos = (int) (Math.cos(Math.toRadians(pos[2]) + 0.5));
+    			//  if (sin == 0) {
+    				  //y
+    				//  odometer.setXYT(pos[0], lastPos[1] + sin * LINE_SPACING, pos[2]);
+    			//  } else {
+    			//	  //x change
+    			//	  odometer.setXYT(lastPos[1] + cos * LINE_SPACING, pos[1], pos[2]);
+    			 
     			  }
     		  }
     	  }
-      }
+      
+      //unneccessary in new code because checks error threshold
       //If we've turned dramatically, this wont be useful, so nullify last data
-      try {
-    	  if (lastPos != null && Math.abs(odometer.getXYT()[2] - lastPos[2]) > T_THRESHOLD) {
-    		  lastPos = null;
-    	  }
-      } catch (NullPointerException npe) {}
+      //try {
+    //	  if (lastPos != null && Math.abs(odometer.getXYT()[2] - lastPos[2]) > T_THRESHOLD) { //check this
+    //		  lastPos = null;
+   // 	  }
+   //   } catch (NullPointerException npe) {//do nothing
+   // 	  }
+      
+    //  Lab2.lcd.drawString(lineCount + " line(s) detected", 0, 5);
      
-      Lab2.lcd.drawString(lineCount + " line(s) detected", 0, 5);
-
-
       // this ensure the odometry correction occurs only once every period
       correctionEnd = System.currentTimeMillis();
       if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
@@ -137,14 +166,21 @@ public class OdometryCorrection implements Runnable {
           // there is nothing to be done here
         }
       }
+      
     }
-  }
+      
+ 
+  }//end run method
   
-  //calculates if current distance is within distance error threshold
+  
+  //calculates distance between two positions
   private static double dist(double[] a, double[] b) {
 	  if (a.length < 2 || b.length < 2) {
 		  return -1;
 	  }
 	  return Math.sqrt(Math.pow(a[0]-b[0], 2) + Math.pow(a[1]-b[1],2)); //fixed distance formula
   }
+  
+ 
+   
 }
